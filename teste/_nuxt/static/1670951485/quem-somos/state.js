bbca58073cba9bedaa0c7e37bc1a32de88664d99
Daniel <danivictor45i@gmail.com>
2022-12-13 14:30:17 -0300

    fix

---
+++ teste/_nuxt/static/1670951485/quem-somos/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F\u002F_nuxt\u002Fstatic\u002F1670951485",layout:"default",error:a,serverRendered:true,routePath:"\u002Fquem-somos",config:{_app:{basePath:"\u002F\u002F",assetsPath:"\u002F\u002F_nuxt\u002F",cdnURL:a}}}}(null));
